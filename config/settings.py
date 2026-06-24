26fbbf8e2667453cba0d21d48b3562c6cc7a6fda
grgsaugat11 <saugat987.sg@gmail.com>
2026-06-24 13:41:26 +0545

    Update EMAIL_PORT configuration in settings.py to use port 2525

--- config/settings.py
+++ config/settings.py
@@ -184,7 +184,7 @@ if DEBUG:
 else:
     EMAIL_BACKEND = config("EMAIL_BACKEND", default="django.core.mail.backends.smtp.EmailBackend")
     EMAIL_HOST = config("EMAIL_HOST", default="smtp-relay.brevo.com")
-    EMAIL_PORT = config("EMAIL_PORT", default=587, cast=int)
+    EMAIL_PORT = config("EMAIL_PORT", default=2525, cast=int)
     EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=True, cast=bool)
     EMAIL_HOST_USER = config("EMAIL_HOST_USER")
     EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
